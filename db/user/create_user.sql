c8531cfae38e81505e8651616ac9ff439c03693e
jacoborbach <jacoborbach96@gmail.com>
2021-01-19 14:29:17 -0500

    first commit

---
+++ db/user/create_user.sql
@@ -0,0 +1,3 @@
+insert into helo_users (username, password, profile_pic)
+VALUES ($1,$2,$3)
+returning *;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 require('dotenv').config();
 const express = require('express'),
-      userCtrl = require('./controllers/user'),
-      postCtrl = require('./controllers/posts')
+    massive = require('massive'),
+    session = require('express-session'),
+    { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = proccess.env,
+    userCtrl = require('./controllers/user'),
+    postCtrl = require('./controllers/posts')
 
 
 const app = express();
 
 app.use(express.json());
+
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 }
+}));
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected!')
+})
+
 
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
@@ -20,4 +39,4 @@ app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
 
-app.listen(4000, _ => console.log(`running on ${4000}`));
+app.listen(SERVER_PORT, _ => console.log(`running on ${SERVER_PORT}`));
